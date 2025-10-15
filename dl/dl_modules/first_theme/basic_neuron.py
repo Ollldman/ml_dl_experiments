d678af492613d65f3696fe29d3c926f860a9a182
Ollldman <dodrolla@gmail.com>
2025-10-15 22:07:18 +0300

    added basic neuron and relu activation

---
+++ dl/dl_modules/first_theme/basic_neuron.py
@@ -0,0 +1,14 @@
+from typing import Iterable
+from matplotlib.pylab import ArrayLike
+import numpy as np
+
+
+from basic_relu import relu
+
+
+def neuron(inputs: ArrayLike, weights: ArrayLike, bias: float)-> float:
+    # Вычисляем через скалярное произведение вектора весов и входа, прибавляем смещение, возвращаем  единственное число от 0 до +inf
+    total = np.dot(inputs, weights) + bias
+    # Примените ReLU
+    output = relu(total)
+    return output
---
+++ dl/dl_modules/first_theme/basic_relu.py
@@ -0,0 +1,5 @@
+import numpy as np
+
+
+def relu(x: float)-> float:
+    return np.maximum(0, x)
