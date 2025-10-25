2785396601365ee32e66de3bfcb3b1b8bf78ff14
Ollldman <dodrolla@gmail.com>
2025-10-25 18:36:50 +0300

    Updated project structure Added poetry-settings with .env

---
+++ ml_dl_experiments/settings.py
@@ -0,0 +1,20 @@
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+from dotenv import load_dotenv
+
+env_file: str = ".env"
+load_dotenv(env_file)
+
+
+
+class Settings(BaseSettings):
+    model_config=SettingsConfigDict(
+        env_file=env_file,
+        env_file_encoding="utf-8")
+    
+    SOURCE_PATH: str
+
+
+
+
+settings: Settings = Settings() # type:ignore
