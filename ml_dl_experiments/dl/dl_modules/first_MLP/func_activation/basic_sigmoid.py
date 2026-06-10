81582f615f0c6e2f6fc9fdff7a5385009c8d2b3b
Ollldman <dodrolla@gmail.com>
2026-06-10 12:41:00 +0300

    added new from first theme added activations

--- ml_dl_experiments/dl/dl_modules/first_MLP/func_activation/basic_sigmoid.py
+++ ml_dl_experiments/dl/dl_modules/first_MLP/func_activation/basic_sigmoid.py
@@ -1,6 +1,5 @@
 import numpy as np
 from numpy.typing import ArrayLike, NDArray
-from typing import Union
 
 def sigmoid(x: ArrayLike) -> NDArray[np.float64]:
     """
--- ml_dl_experiments/settings.py
+++ ml_dl_experiments/settings.py
@@ -1,22 +1,20 @@
 from pydantic_settings import BaseSettings, SettingsConfigDict
-
+from pathlib import Path
 from dotenv import load_dotenv
 
-env_file: str = ".env"
-load_dotenv(env_file)
+# Загрузка файла конфигураций окружения
+CURRENT_DIR = Path(__file__).resolve().parent
+PROJECT_ROOT = CURRENT_DIR.parent
+ENV_FILE_PATH = PROJECT_ROOT / ".env"
+load_dotenv(ENV_FILE_PATH)
 
-
-
+# Создание валидационного объекта pydantic
 class Settings(BaseSettings):
     model_config=SettingsConfigDict(
-        env_file=env_file,
+        env_file=ENV_FILE_PATH,
         env_file_encoding="utf-8")
-    
     SOURCE_PATH: str
     KAGGLE_USERNAME: str
     KAGGLE_KEY: str
-
-
-
 
 settings: Settings = Settings() # type:ignore
